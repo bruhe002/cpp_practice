b10a609c17bd69acb7902e8042cb92a97a3b8c9a
Eric Bruhns <bruhe002@gmail.com>
2024-01-28 19:29:28 -0700

    Made the exclaim function

--- string_function.cpp
+++ string_function.cpp
@@ -4,3 +4,27 @@
 // Author: Eric Bruhns
 //
 ///////////////////////////////////////////////////
+
+#include <string>
+#include <cctype>
+
+// Define exclaim function
+
+/*
+    Takes a string and returns the modified string with
+    all punctuation switched with a '!'
+
+    @param old_str: string to be modified
+    @return old_str
+*/
+
+std::string exclaim(std::string& old_str) {
+    // Loop through the string
+    for(auto& c : old_str) {
+        if(ispunct(c)) {
+            c = '!';
+        }
+    }
+
+    return old_str;
+}
