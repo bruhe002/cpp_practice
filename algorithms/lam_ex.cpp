876bf02d1358a5ca60a33528855efecf90f3d6ed
Eric Bruhns <bruhe002@gmail.com>
2024-03-01 13:11:32 -0700

    Added comment for clarity

--- algorithms/lam_ex.cpp
+++ algorithms/lam_ex.cpp
@@ -13,6 +13,8 @@ int main() {
 
     int x{42}, y{99}, z{0};
 
+    // This will create an object of the lambda expression 
+    // and the variables are essentially member variables which will retain their values
     auto lam = [=, &z]() mutable { ++x; ++y; z = x + y; };
 
     lam();
