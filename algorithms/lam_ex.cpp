c93b84541ae59096a878e565c91e193d3ff697f8
Eric Bruhns <bruhe002@gmail.com>
2024-03-01 13:06:36 -0700

    First test

--- algorithms/lam_ex.cpp
+++ algorithms/lam_ex.cpp
@@ -10,5 +10,17 @@
 using namespace std;
 
 int main() {
+
+    int x{42}, y{99}, z{0};
+
+    auto lam = [=, &z]() mutable { ++x; ++y; z = x + y; };
+
+    lam();
+    lam();
+
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+    cout << "z = " << z << endl;
+
     return 0;
 }
