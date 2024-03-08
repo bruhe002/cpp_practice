a13e7bd735e36890d1d332b62116386ff66d47fc
Eric Bruhns <bruhe002@gmail.com>
2024-03-08 15:35:17 -0700

    Input exercises

--- algorithms/algorithms_workshop.cpp
+++ algorithms/algorithms_workshop.cpp
@@ -14,12 +14,13 @@
 
 using namespace std;
 
-void printVectors(const vector<int> v) {
-    copy(v.begin(), v.end(), ostream_iterator<int>(cout, " "));
+template<typename T>
+void printVectors(const vector<T> v) {
+    copy(v.begin(), v.end(), ostream_iterator<T>(cout, " "));
     // for (auto e : v) {
     //     cout << e << ", ";
     // }
-    // cout << endl;
+    cout << endl;
 }
 
 /*
@@ -41,19 +42,62 @@ void randFill(vector<int>& v) {
 }
 
 /*
-    Exercise two: Find Maximum value of the vector
+    Exercise Two: Find Maximum value of the vector
+    Exercise Three: Find Maximum Value Index
 
     @param: v - vector to be searched
+    @return: pair<int,int> - first value will be maximum value, second value will be index
 */
-int findMax(const vector<int>& v) {
+pair<int, int> findMax(const vector<int>& v) {
     if (v.size() == 0) {
         cerr << "There is no max in this vector\n";
-        return -1;
+        return make_pair(-1,-1);
     } else {
-        auto max = max_element(v.begin(), v.end());
-
-        return *max;
+        auto max_num = max_element(v.begin(), v.end());
+        int max_idx = distance(v.begin(), max_num); 
+        return make_pair(*max_num, max_idx);
     }
+}
+
+/*
+    Exercise Four: Sum the elements of the vectors
+
+    @param v: vector to sum
+    @return: int
+*/
+int sumVector(const vector<int>& v) {
+    return accumulate(v.begin(), v.end(), 0);
+}
+
+/*
+    Exercise Five: Count the number of odd numbers
+    @param v: vector to parse
+    @return odd number count
+*/
+int countOdds(const vector<int> v) {
+    vector<int> new_v;
+
+    copy_if(v.begin(), v.end(), back_inserter(new_v),
+        [&new_v](int n) { return n % 2 == 1; }
+    );
+
+    return new_v.size();
+}
+/*
+    Exercise Six: Normalize the vector
+    @param v: vector to normalize
+    @return a vector with normalized results
+*/
+vector<double> normalize(const vector<int>& v) {
+    vector<double> ret_v;
+
+    pair<int, int> max_values = findMax(v);
+    copy(v.begin(), v.end(), back_inserter(ret_v));
+    transform(ret_v.begin(), ret_v.end(), ret_v.begin(), 
+        [&max_values](double d) { return d / max_values.first; }
+    );
+
+    return ret_v;
 }
 
 int main() {
@@ -65,10 +109,21 @@ int main() {
     printVectors(vect);
 
     // Ex. 2
-    int max_value = findMax(vect);
-    if(max_value > -1) {
-        cout << "Max Value: = " << max_value << endl;
+    // Ex. 3
+    pair<int, int> max_values = findMax(vect);
+    if(max_values.first != -1) {
+        cout << "Max Value = " << max_values.first << endl;
+        cout << "Max Value Index = " << max_values.second << endl;
     }
+
+    // Ex. 4
+    cout << "Sum of values = " << sumVector(vect) << endl;
+
+    // Ex. 5
+    cout << "Num of Odd Numbers = " << countOdds(vect) << endl;
+
+    // Ex. 6
+    printVectors(normalize(vect));
 
     return 0;
 }
