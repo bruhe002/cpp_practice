ee05993f93be25a3098c9ba80e24d6fb3b5538c1
Eric Bruhns <bruhe002@gmail.com>
2024-03-16 15:06:48 -0600

    Commented out main

--- url_class.cpp
+++ url_class.cpp
@@ -14,7 +14,7 @@ using namespace std;
 class Url {
 
 public:
-    Url(string& protocol, string& resource) {
+    Url(const string& protocol, const string& resource) {
         this->protocol = protocol;
         this->resource = resource;
     }
@@ -22,21 +22,23 @@ public:
     void displayUrl() {
         cout << protocol << "://" << resource;
     }
+
+    ~Url() {}
 private:
     string protocol;
     string resource;
 };
 
-int main() {
-    // Testing URL class
-    // Make string variables
-    string protocol = "http";
-    string resource = "www.youtube.com";
-    // Instantiate an object
-    Url test_addr(protocol, resource);
+// int main() {
+//     // Testing URL class
+//     // Make string variables
+//     string protocol = "http";
+//     string resource = "www.youtube.com";
+//     // Instantiate an object
+//     Url test_addr(protocol, resource);
 
-    // Test method
-    test_addr.displayUrl();
+//     // Test method
+//     test_addr.displayUrl();
 
-    return 0;
-}
+//     return 0;
+// }
