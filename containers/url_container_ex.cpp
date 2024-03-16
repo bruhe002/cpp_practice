960add49efb8ead6247cd3e3c7a5ac0a94d9175e
Eric Bruhns <bruhe002@gmail.com>
2024-03-16 15:06:37 -0600

    Started UrlHistory class

--- containers/url_container_ex.cpp
+++ containers/url_container_ex.cpp
@@ -12,7 +12,32 @@
 
 using namespace std;
 
+class UrlHistory {
+public:
+    UrlHistory() {
+        deque<string> urls{};
+    };
+
+    UrlHistory(UrlHistory& uh) {
+        this->urls = uh.urls;
+    }
+
+    void add_url(const string& pro, const string& res) const {
+        Url website(pro, res);
+
+        urls.push_front(Url(pro, res));
+    }
+
+    ~UrlHistory() {
+        this->urls.clear();
+    }
+
+private:
+    deque<Url> urls;
+};
+
 int main() {
+
 
 
     return 0;
