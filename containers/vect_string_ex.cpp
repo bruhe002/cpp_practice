405097113f051e8e1c3896353c2e38bfbdb73207
Eric Bruhns <bruhe002@gmail.com>
2024-03-16 14:04:14 -0600

    Add comments to vector ex

--- containers/vect_string_ex.cpp
+++ containers/vect_string_ex.cpp
@@ -15,6 +15,8 @@ int main() {
     // Obtain string from User
     string input{""};
     vector<string> strings;
+
+    // Loop through input
     do {
         cout << "Please enter a string: (Enter '-1' to quit)\n";
 
@@ -25,6 +27,7 @@ int main() {
         }
     } while(input != "-1");
 
+    // Display the input
     cout << "Here is the a list of your strings:\n";
     for(int i = 0; i < strings.size(); ++i) {
         cout << i + 1 << ": " << strings[i] << endl;
