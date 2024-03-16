9897ecb5e9ee81151da00ba5fa2734d6281d4594
Eric Bruhns <bruhe002@gmail.com>
2024-03-16 14:32:50 -0600

    Reverse deque finished

---
+++ containers/reverse_deque_string_ex.cpp
@@ -0,0 +1,39 @@
+///////////////////////////////////////////////////
+// @reverse_deque_string_ex.cpp
+//
+// Author: Eric Bruhns
+//
+///////////////////////////////////////////////////
+
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main() {
+
+    // Obtain string from User
+    string input{""};
+    deque<string> strings;
+
+    // Loop through input
+    do {
+        cout << "Please enter a string: (Enter '-1' to quit)\n";
+
+        getline(cin, input);
+
+        if (input != "-1") {
+            strings.push_front(input);
+        }
+    } while(input != "-1");
+
+    // Display the input
+    cout << "Here is the deque of your strings:\n";
+    for(int i = 0; i < strings.size(); ++i) {
+        auto it = strings.begin();
+        advance(it, i);
+        cout << i + 1 << ": " << *it << endl;
+    }
+
+    return 0;
+}
