3655920917cc732cfd2148d0dbb99b78cb7405da
Eric Bruhns <bruhe002@gmail.com>
2024-03-16 13:50:11 -0600

    First test in vect string assignment

--- containers/vect_string_ex.cpp
+++ containers/vect_string_ex.cpp
@@ -13,6 +13,22 @@ using namespace std;
 int main() {
 
     // Obtain string from User
+    string input{""};
+    vector<string> strings;
+    do {
+        cout << "Please enter a string: (Enter '-1' to quit)\n";
+
+        getline(cin, input);
+
+        if (input != "-1") {
+            strings.push_back(input);
+        }
+    } while(input != "-1");
+
+    cout << "Here is the a list of your strings:\n";
+    for(int i = 0; i < strings.size();) {
+        cout << ++i << ": " << strings[i] << endl;
+    }
 
     return 0;
 }
