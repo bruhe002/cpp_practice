493a29de86f2f6b5ea76e7bb9b3180ea3dfe6267
Eric Bruhns <bruhe002@gmail.com>
2024-03-28 23:29:32 -0600

    Finished map ex

--- containers/maps_container_ex.cpp
+++ containers/maps_container_ex.cpp
@@ -6,8 +6,30 @@
 ///////////////////////////////////////////////////
 
 #include <iostream>
+#include <map>
+#include <utility>
+
+using namespace std;
 
 int main() {
+
+    // Create a container map
+    map<string, int> m;
+    // Ask for user input
+    string input{""};
+    do {
+        cout << "Please Enter a word:\n";
+        cin >> input;
+
+        if(input != "-1"){
+            m.insert(pair<string, int>(input, input.size()));
+        }
+    } while(input != "-1");
+
+    for(auto p : m) {
+        cout << "The words you entered:\n";
+        cout << p.first << " has a size of " << p.second << endl;
+    }
 
     return 0;
 }
