fe99b932606c28ec6b5d242f523cdefcc2e6c909
Eric Bruhns <bruhe002@gmail.com>
2024-03-29 23:36:00 -0600

    Main file skeleton code

---
+++ containers/mastermind_game_files/main.cpp
@@ -0,0 +1,37 @@
+///////////////////////////////////////////////////
+// @main.cpp
+//
+// Author: Eric Bruhns
+//
+///////////////////////////////////////////////////
+
+#include <iostream>
+#include <map>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // peg_array pegs = populate();
+
+    // bool winner{false};
+    // for(int i = 0; i < constants::nturns; ++i) {
+    //     peg_array input = get_input();
+
+    //     auto exact = exact_matches(input, pegs);
+    //     auto loose = loose_matches(input, pegs) - exact;
+    //     cout << exact << " exact match(es)\n";
+    //     cout << loose << " near match(es)\n";
+
+    //     if (exact == constants::npegs) {
+    //         cout << "You have found the correct solution! ";
+    //         for (auto peg : pegs) cout << peg;
+    //         cout << endl;
+    //         winner = true;
+    //         break;
+    //     }
+    // }
+
+    return 0;
+}
