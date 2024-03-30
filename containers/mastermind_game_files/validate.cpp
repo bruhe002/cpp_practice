5f12d945660a25d15d1b1ec7999e68d91489035a
Eric Bruhns <bruhe002@gmail.com>
2024-03-30 00:17:45 -0600

    finish validate array

--- containers/mastermind_game_files/validate.cpp
+++ containers/mastermind_game_files/validate.cpp
@@ -24,3 +24,28 @@ int exact_matches(const peg_array& guess, const peg_array& solution) {
         [](const char c) { return c != '\0'; }
     );
 }
+
+// Find number of loose matches
+int loose_matches(const peg_array& guess, const peg_array& solution) {
+    string copy_guess;
+
+    // Counter to store the number of loose matches
+    int count{0};
+
+    // Make a copy of the user's guess
+    copy(begin(guess), end(guess), back_inserter(copy_guess));
+
+    for(auto color = begin(solution); color != end(solution); ++color) {
+        auto it = find_if(begin(copy_guess), end(copy_guess),
+            [=](const char c) { return c == *color; }
+        );
+
+        if(it != end(copy_guess)) {
+            ++count;
+            copy_guess.erase(it);
+        }
+    }
+
+    return count;
+
+}
