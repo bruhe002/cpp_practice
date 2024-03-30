e28efa56ad2abb17a492451f8ed7803f812aff08
Eric Bruhns <bruhe002@gmail.com>
2024-03-30 00:28:56 -0600

    Include constants

--- containers/mastermind_game_files/main.cpp
+++ containers/mastermind_game_files/main.cpp
@@ -6,44 +6,52 @@
 ///////////////////////////////////////////////////
 
 #include <iostream>
-#include <map>
-#include <utility>
-#include <vector>
+#include <algorithm>
+#include <random>
 
 #include "constants.h"
+#include "validate.h"
+#include "inputs.h"
 
 using namespace std;
 
-// peg_array populate() {
-//     peg_array code{0};
-//     static mt19937 mt;
-//     uniform_int_distribution<int> ud(0, constants::ncolors-1);
+peg_array populate() {
+    peg_array code{0};
+    static mt19937 mt;
+    uniform_int_distribution<int> ud(0, constants::ncolors-1);
 
-//     generate(begin(code), end(code),
-//         [&]() { return constants::colors[ud(mt)]; }
-//     );
-// }
+    generate(begin(code), end(code),
+        [&]() { return constants::colors[ud(mt)]; }
+    );
+}
 
 int main() {
-    // peg_array pegs = populate();
+    peg_array pegs = populate();
 
-    // bool winner{false};
-    // for(int i = 0; i < constants::nturns; ++i) {
-    //     peg_array input = get_input();
+    bool winner{false};
+    for(int i = 0; i < constants::nturns; ++i) {
+        peg_array input = get_input();
 
-    //     auto exact = exact_matches(input, pegs);
-    //     auto loose = loose_matches(input, pegs) - exact;
-    //     cout << exact << " exact match(es)\n";
-    //     cout << loose << " near match(es)\n";
+        auto exact = exact_matches(input, pegs);
+        auto loose = loose_matches(input, pegs) - exact;
+        cout << exact << " exact match(es)\n";
+        cout << loose << " near match(es)\n";
 
-    //     if (exact == constants::npegs) {
-    //         cout << "You have found the correct solution! ";
-    //         for (auto peg : pegs) cout << peg;
-    //         cout << endl;
-    //         winner = true;
-    //         break;
-    //     }
-    // }
+        if (exact == constants::npegs) {
+            cout << "You have found the correct solution! ";
+            for (auto peg : pegs) cout << peg;
+            cout << endl;
+            winner = true;
+            break;
+        }
+    }
+    if(!winner) {
+        cout << "Sorry! The solution was:";
+        for(auto peg : pegs) {
+            cout << peg;
+        }
+        cout << endl;
+    }
 
     return 0;
 }
