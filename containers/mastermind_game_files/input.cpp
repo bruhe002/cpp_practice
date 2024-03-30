820647d1ae139bdd3a81f508a2f321c6aa1625d5
Eric Bruhns <bruhe002@gmail.com>
2024-03-29 23:56:13 -0600

    Add input functions

---
+++ containers/mastermind_game_files/input.cpp
@@ -0,0 +1,46 @@
+///////////////////////////////////////////////////
+// @input.cpp
+//
+// Author: Eric Bruhns
+//
+///////////////////////////////////////////////////
+
+#include <iostream>
+#include <algorithm>
+
+#include "constants.h"
+#include "inputs.h"
+
+using namespace std;
+
+string normalize(const string& s) {
+    string retval{""};
+
+    copy_if(s.begin(), s.end(), back_inserter(retval),
+        [](char c) { return isalpha(c); }
+    );
+
+    transform(retval.begin(), retval.end(), begin(retval),
+        [](char c) { return toupper(c); }
+    );
+}
+
+peg_array get_input() {
+    string input{""}, normal{""};
+    peg_array result{""};
+
+    do {
+        cout << "Enter " << constants::npegs << " characters (";
+        for(auto c: constants::colors)
+            cout << c;
+        cout << ")" << endl;
+
+        normal = normalize(input);
+    } while (normal.size() != constants::npegs);
+
+    for(int i = 0; i < normal.size(); i++) {
+        result[i] = normal[i];
+    }
+
+    return result;
+}
