810a98365e6ef8a086ef124c93589f88e07e855b
Eric Bruhns <bruhe002@gmail.com>
2024-03-31 10:43:38 -0600

    Made public member functions

--- inheritance_polymorphism/inheritance_ex.cpp
+++ inheritance_polymorphism/inheritance_ex.cpp
@@ -10,16 +10,29 @@
 using namespace std;
 
 class Parent {
-
+public:
+    void print() const {
+        cout << "This is a Parent.\n";
+    }
 };
 
 class Child : public Parent {
-
+public:
+    void print() const {
+        cout << "This is a Child.\n";
+    }
 };
 
 class Grandchild : public Child {
-
+public:
+    void print() const {
+        cout << "This is a Grandchild.\n";
+    }
 };
+
+void print_class(const Parent& p) {
+    p.print();
+}
 
 int main() {
 
