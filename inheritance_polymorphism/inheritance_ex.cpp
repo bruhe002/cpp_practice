78d03d8a5cc579805d02224f0e5b69fab2b00a81
Eric Bruhns <bruhe002@gmail.com>
2024-03-31 10:31:37 -0600

    Inheritance skeleton code

---
+++ inheritance_polymorphism/inheritance_ex.cpp
@@ -0,0 +1,27 @@
+///////////////////////////////////////////////////
+// @inheritance_ex.cpp
+//
+// Author: Eric Bruhns
+//
+///////////////////////////////////////////////////
+
+#include <iostream>
+
+using namespace std;
+
+class Parent {
+
+};
+
+class Child : public Parent {
+
+};
+
+class Grandchild : public Child {
+
+};
+
+int main() {
+
+    return 0;
+}
